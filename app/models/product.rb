696542d5d16bcdd5cc5c191c30a292d2770aae52
Takaaki Sasaki <takaaki.sasaki06@gmail.com>
2019-03-15 17:18:06 +0900

    priceにバリデーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, :desc, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
   belongs_to :user
   has_many :investments
-  validates :title, :desc, presence: true
 end
