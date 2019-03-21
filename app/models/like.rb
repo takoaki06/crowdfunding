f645dc85b57b091f962a70905a7f1da197364266
Takaaki Sasaki <takaaki.sasaki06@gmail.com>
2019-03-21 11:34:24 +0900

    uniqueness_ofからuniquenessに変更

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :product
-  validates_uniqueness_of :product_id, scope: :user_id
+  validates :product_id, uniqueness: { scope: :user_id }
 end
