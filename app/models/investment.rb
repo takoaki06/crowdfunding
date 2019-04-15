fca35c06d771bf8224f013dd2b44449b47d0b451
Takaaki Sasaki <takaaki.sasaki06@gmail.com>
2019-04-15 16:36:39 +0900

    出資に制限をかけるメソッドを追加

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -3,4 +3,15 @@ class Investment < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true }
   belongs_to :user
   belongs_to :product
+
+  #目標額と出資額の合計の判別
+  def self.investable?(product)
+    investment_sum_price = product.investments.sum(:price)
+    product.price > investment_sum_price
+  end
+
+  #目標額と出資額の判別
+  def exceed?(product)
+    product.price >= self.price
+  end
 end
