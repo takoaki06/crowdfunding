462b74fb5c4f2cde55dc8ca7d8008ac4fc006c7a
Takaaki Sasaki <takaaki.sasaki06@gmail.com>
2019-05-07 18:47:56 +0900

    context修正

--- spec/models/investment_spec.rb
+++ spec/models/investment_spec.rb
@@ -4,12 +4,9 @@ RSpec.describe "Investment", type: :model do
 
   describe "#investable?" do
     context '出資の合計が目標額に達していない場合' do
-      let!(:user_a) { FactoryBot.create(:user) }
-      let!(:product) { FactoryBot.create(:product, user: user_a) }
-      let!(:investment_a) { FactoryBot.create(:investment, user: user_a, product: product) }
-
-      let!(:user_b) { FactoryBot.create(:user) }
-      let!(:investment_b) { FactoryBot.create(:investment, user: user_b, product: product) }
+      let!(:user) { FactoryBot.create(:user) }
+      let!(:product) { FactoryBot.create(:product, user: user) }
+      let!(:investment) { FactoryBot.create(:investment, user: user, product: product) }
 
       it 'trueを返すこと' do
         expect(Investment.investable?(product)).to be true
@@ -17,12 +14,9 @@ RSpec.describe "Investment", type: :model do
     end
 
     context '出資の合計が目標額に達している場合' do
-      let!(:user_a) { FactoryBot.create(:user) }
-      let!(:product) { FactoryBot.create(:product, user: user_a) }
-      let!(:investment_a) { FactoryBot.create(:investment, user: user_a, product: product, price: 10000) }
-
-      let!(:user_b) { FactoryBot.create(:user) }
-      let!(:investment_b) { FactoryBot.create(:investment, user: user_b, product: product, price: 10000) }
+      let!(:user) { FactoryBot.create(:user) }
+      let!(:product) { FactoryBot.create(:product, user: user) }
+      let!(:investment) { FactoryBot.create(:investment, user: user, product: product, price: 100000) }
 
       it 'falseを返すこと' do
         expect(Investment.investable?(product)).to be false
@@ -31,7 +25,7 @@ RSpec.describe "Investment", type: :model do
   end
 
   describe "#exceed?" do
-    context '出資が目標額より低い場合' do
+    context '出資が目標額に達していない場合' do
       let!(:user) { FactoryBot.create(:user) }
       let!(:product) { FactoryBot.create(:product, user: user) }
       let!(:investment) { FactoryBot.create(:investment, user: user, product: product) }
@@ -41,7 +35,7 @@ RSpec.describe "Investment", type: :model do
       end
     end
 
-    context '出資が目標額以上の場合' do
+    context '出資が目標額に達している場合' do
       let!(:user) { FactoryBot.create(:user) }
       let!(:product) { FactoryBot.create(:product, user: user) }
       let!(:investment) { FactoryBot.create(:investment, user: user, product: product, price: 200000) }
