5eb6a644580e2ed826884e507c89020ef5d57688
Takaaki Sasaki <takaaki.sasaki06@gmail.com>
2019-05-15 16:31:12 +0900

    UsersController追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def index
@@ -12,7 +13,21 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
   
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :profile, category_ids: [])
+    end
  
 end
