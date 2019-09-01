7604756940358ae016de8e3e7b57739ccdf00f50
Takaaki Sasaki <takaaki.sasaki06@gmail.com>
2019-09-01 17:32:21 +0900

    [add]guest user

--- db/fixtures/01_user.rb
+++ db/fixtures/01_user.rb
@@ -1,4 +1,5 @@
 User.seed(:id,
   { id: 1, name: "user1", profile: "user1です。", email: "user1@example.com", password: "password1", role: :administer },
-  { id: 2, name: "user2", profile: "user2です。", email: "user2@example.com", password: "password2", role: :general }
+  { id: 2, name: "user2", profile: "user2です。", email: "user2@example.com", password: "password2", role: :general },
+  { id: 3, name: "guest", profile: "guestです。", email: "guest@example.com", password: "guestpassword", role: :general }
 )
