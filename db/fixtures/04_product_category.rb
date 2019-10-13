ebbe83a3177767f572d445ba292dc346f89cc8c2
Takaaki Sasaki <takaaki.sasaki06@gmail.com>
2019-10-13 11:50:25 +0900

    seed-fuの初期データを修正

--- db/fixtures/04_product_category.rb
+++ db/fixtures/04_product_category.rb
@@ -1,11 +1,8 @@
-product_ids = [*1..20]
-category_ids = [2,5,3,2,3,4,5,1,1,2,4,3,1,3,2,4,3,4,5,1]
-
-20.times do |index|
+Product.all.each do |product|
   ProductCategory.seed(:id,
     {
-      product_id: product_ids[index],
-      category_id: category_ids[index]
+      product_id: product.id,
+      category_id: Category.all.ids.sample
     }
   )
 end
